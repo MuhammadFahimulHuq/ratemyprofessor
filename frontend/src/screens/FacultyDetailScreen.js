0ac8ed07b32b7428faf395fa3c14050568b0d1a8
fahim1996112016 <fahimul.huq85@gmail.com>
2022-12-05 23:52:59 +0600

    upload

--- frontend/src/screens/FacultyDetailScreen.js
+++ frontend/src/screens/FacultyDetailScreen.js
@@ -21,7 +21,7 @@ export const FacultyDetailScreen = () => {
       <Row>
         <Col className="d-flex flex-column col-3">
         <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png"  alt=""/>
-        <Button variant="outline-primary" className="mt-1">Edit Photo</Button>
+        <Button variant="outline-primary" className="mt-1">Upload Photo</Button>
         </Col>
         <Col>
          <p>{faculty.name}</p>   
